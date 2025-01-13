bca814919023036803b49048c4b82cf3af74ecec
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-13 12:57:22 +0100

    feat: add ref email, password variables

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,25 @@
 <template>
-    <form>
-        <div class="container">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
-
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
-
-            <button type="submit">Login</button>
-        </div>
+    <form @submit.prevent="handleLogin">
+      <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input v-model="email" type="text" placeholder="Enter Username" name="uname" required />
+  
+        <label for="psw"><b>Password</b></label>
+        <input v-model="password" type="password" placeholder="Enter Password" name="psw" required />
+  
+        <button type="submit">Login</button>
+      </div>
     </form>
-</template>
+  </template>
 
 <script lang="ts">
+import { loginUser } from '@/apis/loginApi';
+import { ref } from 'vue';
+const email = ref('');
+const password = ref('');
+
+const handleLogin = async () => {
     
+}
+
 </script>
