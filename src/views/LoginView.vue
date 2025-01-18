bd79b1b150d35edb69355ae00bac370a35674e53
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-18 15:31:57 +0100

    refactor: set email instead of username

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,12 +6,12 @@
     >
       <h1 class="text-2xl font-bold text-gray-800 text-center">Login</h1>
       <div class="space-y-2">
-        <label for="username" class="block text-sm font-medium text-gray-600"> Username </label>
+        <label for="email" class="block text-sm font-medium text-gray-600"> Email </label>
         <input
           type="text"
-          id="username"
-          v-model="username"
-          placeholder="Enter your username"
+          id="email"
+          v-model="email"
+          placeholder="Enter your email"
           class="w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 text-gray-600 focus:outline-none"
           required
         />
@@ -23,6 +23,26 @@
           id="password"
           v-model="password"
           placeholder="Enter your password"
+          class="w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 text-gray-600 focus:outline-none"
+          required
+        />
+      </div>
+      <div class="flex items-center space-x-2">
+        <input
+          type="checkbox"
+          id="isAdmin"
+          v-model="isAdmin"
+          class="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
+        />
+        <label for="isAdmin" class="text-sm font-medium text-gray-600"> Log in as Admin </label>
+      </div>
+      <div v-if="isAdmin" class="space-y-2">
+        <label for="apiKey" class="block text-sm font-medium text-gray-600"> API Key </label>
+        <input
+          type="text"
+          id="apiKey"
+          v-model="apiKey"
+          placeholder="Enter your API key"
           class="w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 text-gray-600 focus:outline-none"
           required
         />
@@ -43,18 +63,38 @@
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 import ErrorMessage from '@/components/Login/ErrorMessage.vue';
+import { checkEmail, checkPassword, loginAdmin, loginUser } from '@/apis/loginApi';
+import { AxiosError, HttpStatusCode, type AxiosResponse } from 'axios';
 
-const username = ref('');
+const email = ref('');
 const password = ref('');
 const errorMessage = ref('');
+const isAdmin = ref(false);
+const apiKey = ref('');
 const router = useRouter();
 
-const handleLogin = (e: Event) => {
+const handleLogin = async (e: Event) => {
   e.preventDefault();
-  if (username.value === 'admin' && password.value === 'password') {
-    router.push('/home');
-  } else {
-    errorMessage.value = 'Invalid username or password.';
+  if (checkEmail(email.value) && checkPassword(password.value)) {
+    try {
+      let response: AxiosResponse<unknown, unknown>;
+      if (isAdmin.value) {
+        response = await loginAdmin(email.value, password.value, apiKey.value);
+      } else {
+        response = await loginUser(email.value, password.value);
+      }
+      if (response.status === HttpStatusCode.Ok) {
+        router.push('/home');
+      }
+    } catch (error) {
+      if (error instanceof AxiosError && error.response) {
+        if (error.status === HttpStatusCode.Unauthorized) {
+          errorMessage.value = 'Wrong Api Key';
+        }
+      } else {
+        errorMessage.value = 'Invalid username or password.';
+      }
+    }
   }
 };
 </script>
