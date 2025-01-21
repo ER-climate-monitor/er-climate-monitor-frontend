c980ef01ce3856db378c3c39727e76e3287c80a7
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-21 11:39:28 +0100

    feat: add template for sensor list

---
+++ src/views/ListSensorView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sensor-list">
+    <ul>
+      <li v-for="sensor in sensors" :key="sensor.ip">
+        <SensorItem :ip="sensor.ip" :address="sensor.port" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SensorItem from '@/components/List/ListComponent.vue';
+import { ref } from 'vue';
+const sensors = ref([]);
+</script>
