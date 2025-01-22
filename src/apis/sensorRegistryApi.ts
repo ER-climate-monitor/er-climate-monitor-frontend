58e295fe22b5339cc6e236ff23ca2297f59cadbf
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-22 16:02:19 +0100

    feat: add api for deleting an existing sensor

--- src/apis/sensorRegistryApi.ts
+++ src/apis/sensorRegistryApi.ts
@@ -1,17 +1,23 @@
 import { config } from '@/config/config';
+import { SENSOR_IP, SENSOR_PORT } from '@/headers/sensorHeaders';
 import { USER_JWT_TOKEN_HEADER } from '@/headers/userHeaders';
 import axios from 'axios';
 
 const GET_ALL_SENSOR_PATH = config.apiBaseUrl + '/sensor/all';
-// const SHUT_DOWN_SENSOR_PATH = config.apiBaseUrl + '/sensor/shutdown';
+const SHUT_DOWN_SENSOR_PATH = config.apiBaseUrl + '/sensor/shutdown';
 
 async function getAllSensors(token: string) {
   const headers = { [USER_JWT_TOKEN_HEADER]: token };
   return await axios.get(GET_ALL_SENSOR_PATH, { headers });
 }
 
-async function shutDownSensor(token: string) {
-  return token;
+async function shutDownSensorApi(token: string, ip: string, port: number) {
+  const headers = { [USER_JWT_TOKEN_HEADER]: token };
+  const data = { [SENSOR_IP]: ip, [SENSOR_PORT]: port };
+  return await axios.delete(SHUT_DOWN_SENSOR_PATH, {
+    headers,
+    data,
+  });
 }
 
-export { getAllSensors, shutDownSensor };
+export { getAllSensors, shutDownSensorApi };
--- src/views/ListSensorView.vue
+++ src/views/ListSensorView.vue
@@ -2,7 +2,12 @@
   <div class="sensor-list">
     <ul>
       <li v-for="sensor in sensors" :key="sensor.ip">
-        <SensorItem :ip="sensor.ip" :address="sensor.port" />
+        <SensorItem
+          :ip="sensor.ip"
+          :port="sensor.port"
+          :name="sensor.name"
+          @removeSensor="removeSensorFromList"
+        />
       </li>
     </ul>
   </div>
@@ -13,13 +18,19 @@ import SensorItem from '@/components/List/ListComponent.vue';
 import { getAllSensors } from '@/apis/sensorRegistryApi';
 import { ref, onMounted, type Ref } from 'vue';
 import { getToken } from '@/utils/manageToken';
-import { Sensor, type ISensor } from '@/models/sensorModel';
+import { BasicSensor, type Sensor } from '@/models/sensorModel';
 const sensors: Ref<Array<Sensor>> = ref([]);
 onMounted(async () => {
   const token = getToken();
   const response = await getAllSensors(token);
-  response.data.sensors.forEach((element: ISensor) => {
-    sensors.value.push(new Sensor(element.ip, element.port));
+  response.data.sensors.forEach((element: Sensor) => {
+    sensors.value.push(new BasicSensor(element.ip, element.port, element.name || 'unknown'));
   });
 });
+
+const removeSensorFromList = (sensorToRemove: { ip: string; port: number }) => {
+  sensors.value = sensors.value.filter(
+    (sensor) => sensor.ip !== sensorToRemove.ip || sensor.port !== sensorToRemove.port,
+  );
+};
 </script>
