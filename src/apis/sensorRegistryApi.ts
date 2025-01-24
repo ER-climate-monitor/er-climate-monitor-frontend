000b7f77c5d05450d9911b2b875ada8218d89f59
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-24 12:08:38 +0100

    feat: use user header for token

--- src/apis/sensorRegistryApi.ts
+++ src/apis/sensorRegistryApi.ts
@@ -1,22 +1,22 @@
 import { config } from '@/config/config';
 import { SENSOR_IP, SENSOR_PORT } from '@/headers/sensorHeaders';
-import { USER_JWT_TOKEN_HEADER } from '@/headers/userHeaders';
+import { USER_TOKEN_HEADER } from '@/headers/userHeaders';
 import axios from 'axios';
 
 const GET_ALL_SENSOR_PATH = config.apiBaseUrl + '/sensor/all';
 const SHUT_DOWN_SENSOR_PATH = config.apiBaseUrl + '/sensor/shutdown';
 
 async function getAllSensors(token: string) {
-  const headers = { [USER_JWT_TOKEN_HEADER]: token };
-  return await axios.get(GET_ALL_SENSOR_PATH, { headers });
+  const headers = { [USER_TOKEN_HEADER]: token };
+  return await axios.get(GET_ALL_SENSOR_PATH, { headers: headers });
 }
 
 async function shutDownSensorApi(token: string, ip: string, port: number) {
-  const headers = { [USER_JWT_TOKEN_HEADER]: token };
+  const headers = { [USER_TOKEN_HEADER]: token };
   const data = { [SENSOR_IP]: ip, [SENSOR_PORT]: port };
   return await axios.delete(SHUT_DOWN_SENSOR_PATH, {
-    headers,
-    data,
+    headers: headers,
+    params: data,
   });
 }
 
