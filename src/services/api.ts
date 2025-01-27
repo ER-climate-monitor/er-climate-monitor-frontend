28679325a2c76ae91bb23c5d448d3ac371d8989f
Fabio Vincenzi <fabio.vincenzi2001@gmail.com>
2025-01-27 09:47:05 +0100

    feat: add fetchSensorDetections request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import type { SensorLocation } from '@/types/SensorLocation';
+import type { Detection } from '@/types/SensorDetection';
 
 export const fetchSensorLocations = async (sensorType: string): Promise<SensorLocation[]> => {
   if (!sensorType) return [];
@@ -12,6 +13,24 @@ export const fetchSensorLocations = async (sensorType: string): Promise<SensorLo
     return response.data;
   } catch (error) {
     console.error('Error fetching sensor locations:', error);
+    return [];
+  }
+};
+
+export const fetchSensorDetections = async (
+  sensorType: string,
+  sensorId: string,
+): Promise<Detection[]> => {
+  if (!sensorType || !sensorId) return [];
+
+  try {
+    const response = await axios.get<Detection[]>(
+      `http://localhost:3000/v0/sensor/${sensorType.toLowerCase()}/${sensorId}/detections`,
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching sensor detections:', error);
     return [];
   }
 };
