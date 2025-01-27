e7c24c62f0657840dfb0423399f913c10d1fbb12
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-27 12:18:04 +0100

    feat: add update sensor actions and export them

--- src/headers/sensorHeaders.ts
+++ src/headers/sensorHeaders.ts
@@ -2,4 +2,8 @@ const SENSOR_PORT = 'sensorPort';
 const SENSOR_IP = 'sensorIp';
 const SENSOR_NAME = 'sensorName';
 
-export { SENSOR_IP, SENSOR_PORT, SENSOR_NAME };
+const UPDATE_NAME_ACTION = 'update-name';
+const UPDATE_CRONJOB_DAYS = 'update-cronjob-days';
+const UPDATE_CRONJOB_TIME = 'update-cronjob-time';
+
+export { SENSOR_IP, SENSOR_PORT, SENSOR_NAME, UPDATE_CRONJOB_DAYS, UPDATE_CRONJOB_TIME, UPDATE_NAME_ACTION };
