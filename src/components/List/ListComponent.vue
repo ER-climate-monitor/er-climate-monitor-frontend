302bef30db80d13323ec9279a61d3c4d6a7780af
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-27 12:19:00 +0100

    feat: use modal and register on save and close event

--- src/components/List/ListComponent.vue
+++ src/components/List/ListComponent.vue
@@ -7,45 +7,64 @@
             <label class="sensor-label">Port: {{ port }}</label>
         </div>
 
-        <!-- Action Buttons -->
         <div class="sensor-actions">
             <button @click="shutDownSensor" class="action-button shut-off">Shut Down</button>
-            <button @click="openSettings" class="action-button settings">Settings</button>
+            <button @click="toggleModal" class="action-button settings">Settings</button>
         </div>
+
+        <SensorSettingsModal
+            v-if="isModalVisible"
+            :name="name"
+            :days="days"
+            :workingHours="workingHours"
+            @close="toggleModal"
+            @save="saveSettings"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import SensorSettingsModal from '@/components/modal/SensorSettingsModal.vue';
 import { shutDownSensorApi } from '@/apis/sensorRegistryApi';
 import { getToken } from '@/utils/manageToken';
 import { HttpStatusCode } from 'axios';
 import { defineEmits } from 'vue';
+import { UPDATE_CRONJOB_DAYS, UPDATE_NAME_ACTION } from '@/headers/sensorHeaders';
 
 const emit = defineEmits(['removeSensor']);
 
 const props = defineProps({
-    ip: {
-        type: String,
-        default: '',
-    },
-    name: {
-        type: String,
-        default: '',
-    },
-    port: {
-        type: Number,
-        default: 0,
-    },
+    ip: { type: String, default: '' },
+    name: { type: String, default: '' },
+    port: { type: Number, default: 0 },
 });
+
+const isModalVisible = ref(false);
+const days = ref([]); // Assume this comes from the backend
+const workingHours = ref({ hours: 0, minutes: 0 });
+
+const toggleModal = () => {
+    isModalVisible.value = !isModalVisible.value;
+};
+
+const saveSettings = (newSettings: { [key: string]: string }) => {
+    const action = newSettings.action;
+    switch (action) {
+        case UPDATE_NAME_ACTION: {
+            console.log(newSettings.name);
+        }
+        case UPDATE_CRONJOB_DAYS: {
+            console.log(newSettings.days);
+        }
+    }
+};
 
 const shutDownSensor = async () => {
     const response = await shutDownSensorApi(getToken(), props.ip, props.port);
     if (response.status === HttpStatusCode.Ok) {
         emit('removeSensor', { ip: props.ip, port: props.port });
     }
-};
-const openSettings = () => {
-    console.log(props.ip);
 };
 </script>
 
