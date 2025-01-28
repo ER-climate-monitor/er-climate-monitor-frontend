11bb9263c381faf534a9373fde8d6a9a597f87b4
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-28 14:25:25 +0100

    feat: init search engine

--- src/views/ListSensorView.vue
+++ src/views/ListSensorView.vue
@@ -1,15 +1,28 @@
 <template>
-    <div id="sensor list" class="sensor-list">
-        <ul>
-            <li v-for="sensor in sensors" :key="sensor.ip">
-                <SensorItem
-                    :ip="sensor.ip"
-                    :port="sensor.port"
-                    :name="sensor.name"
-                    @removeSensor="removeSensorFromList"
-                />
-            </li>
-        </ul>
+    <div>
+        <div id="search-engine" class="pb-10">
+            <label class="text-lg font-small text-gray-900" for="input-name"> Name: </label>
+            <input
+                id="input-name"
+                type="text"
+                v-model="searchQuery"
+                placeholder="Search a sensor"
+                class="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-900"
+            />
+            <label class="text-lg font-small text-gray-900"> Filter: </label>
+        </div>
+        <div id="sensor list" class="sensor-list">
+            <ul class="overflow-auto">
+                <li v-for="sensor in sensors" :key="sensor.ip">
+                    <SensorItem
+                        :ip="sensor.ip"
+                        :port="sensor.port"
+                        :name="sensor.name"
+                        @removeSensor="removeSensorFromList"
+                    />
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -20,6 +33,8 @@ import { ref, onMounted, type Ref } from 'vue';
 import { getToken } from '@/utils/manageToken';
 import { BasicSensor, type Sensor } from '@/models/sensorModel';
 const sensors: Ref<Array<Sensor>> = ref([]);
+const searchQuery = ref('');
+
 onMounted(async () => {
     const token = getToken();
     const response = await getAllSensors(token);
