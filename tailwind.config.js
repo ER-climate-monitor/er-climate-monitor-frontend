60e86fe3f9bc5ea203dc13472a6ed1e5b4da8b40
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-28 11:50:46 +0100

    feat: add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    darkMode: 'class',
+    content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                pastelViolet: '#574964',
+            },
+        },
+    },
+    plugins: [],
 };
