7d0e235376b7538ca51065ace27db6b207bb9c9f
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-01-28 11:55:52 +0100

    refactor: remove style tag and use teailwind

--- src/components/List/ListComponent.vue
+++ src/components/List/ListComponent.vue
@@ -44,6 +44,7 @@ import {
 import { getToken } from '@/utils/manageToken';
 import { HttpStatusCode } from 'axios';
 import { defineEmits } from 'vue';
+import '@/assets/tailwind.css';
 import { UPDATE_CRONJOB_DAYS, UPDATE_CRONJOB_TIME, UPDATE_NAME_ACTION } from '@/headers/sensorHeaders';
 
 const emit = defineEmits(['removeSensor']);
@@ -106,47 +107,3 @@ const shutDownSensor = async () => {
     }
 };
 </script>
-
-<style scoped>
-.sensor-item {
-    display: flex;
-    flex-direction: column;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    margin: 10px 0;
-}
-
-.sensor-info {
-    margin-bottom: 10px;
-}
-
-.sensor-label {
-    display: block;
-    font-size: 14px;
-    color: #333;
-}
-
-.sensor-actions {
-    display: flex;
-    gap: 10px;
-}
-
-.action-button {
-    padding: 5px 10px;
-    font-size: 14px;
-    border: none;
-    border-radius: 3px;
-    cursor: pointer;
-}
-
-.action-button.shut-off {
-    background-color: #ff4d4d;
-    color: white;
-}
-
-.action-button.settings {
-    background-color: #4d79ff;
-    color: white;
-}
-</style>
