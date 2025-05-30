b2a601856f38180b9b9afccf521d9296a1848cf9
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2025-05-30 14:32:03 +0200

    feat: change base for production

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+  base: process.env.NODE_ENV === 'production'
+    ? '/er-climate-monitor-frontend/'
+    : '/',
   server: {
     proxy: {
       '/v0': {
